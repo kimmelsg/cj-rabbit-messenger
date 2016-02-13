6bce3517763277ccc86659ddd3d355776f62ed0d
Josh Forbes <jforbes@kimmel.com>
2016-02-13 05:21:42 -0500

    Implementing an interface for handlers.

---
+++ src/HandlerInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NavJobs\Transmit;
+
+interface HandlerInterface
+{
+    /**
+     * Implement a method to return the exchange name the handler should listen to.
+     *
+     * @return mixed
+     */
+    public function getExchangeName();
+
+    /**
+     * Implement a method to operate on the message.
+     *
+     * @param $message
+     * @return mixed
+     */
+    public function handle($message);
+
+}
