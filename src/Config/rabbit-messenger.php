2eaa4aad8da3f54409356e8616a7624bb90e83c2
Josh Forbes <jforbes@kimmel.com>
2016-02-13 08:13:57 -0500

    Adding config.

---
+++ src/Config/rabbit-messenger.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'host'     => env('RABBITMQ_HOST', 'localhost'),
+    'port'     => env('RABBITMQ_HOST', '5672'),
+    'user'     => env('RABBITMQ_USER', 'guest'),
+    'password' => env('RABBITMQ_HOST', 'guest'),
+
+];
