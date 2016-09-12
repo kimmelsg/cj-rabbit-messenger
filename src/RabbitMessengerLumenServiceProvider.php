d097508097f5f4bc8609d0ee7ca68bb1af9404b8
Josh Forbes <jforbes@kimmel.com>
2016-09-12 11:19:28 -0400

    Adding a lumen service provider.

---
+++ src/RabbitMessengerLumenServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace NavJobs\RabbitMessenger;
+
+use Illuminate\Support\ServiceProvider;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+class RabbitMessengerLumenServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->configure('rabbit-messenger');
+        $this->mergeConfigFrom(__DIR__.'/../config/rabbit-messenger.php', 'rabbit-messenger');
+
+        $this->app->bind(AMQPStreamConnection::class, function ($app) {
+            return new AMQPStreamConnection(
+                config('rabbit-messenger.host'),
+                config('rabbit-messenger.port'),
+                config('rabbit-messenger.user'),
+                config('rabbit-messenger.password'),
+                $vhost = '/',
+                $insist = false,
+                $login_method = 'AMQPLAIN',
+                $login_response = null,
+                $locale = 'en_US',
+                $connection_timeout = 60,
+                $read_write_timeout = 60,
+                $context = null,
+                $keepalive = false,
+                $heartbeat = 30
+            );
+        });
+
+        $this->app->bind('command.rabbit.consume', function ($app) {
+            return $app['NavJobs\RabbitMessenger\Commands\Consumer'];
+        });
+
+        $this->commands('command.rabbit.consume');
+    }
+}
